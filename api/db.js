bf75f2f07aa6c32af6e4dbfc81507fc11b366c54
Damsara42 <230258519+Damsara42@users.noreply.github.com>
2025-11-26 21:59:52 +0530

    ff

--- api/db.js
+++ api/db.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 
-// Use local MongoDB or your Cloud URL
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://yamemizukimizuki2000_db_user:SABfAchpwXAGGbVQ@streamz.8cqjslk.mongodb.net/?appName=streamz';
+// Use Heroku environment variable or fallback local URI
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/myLocalDB';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log('✅ MongoDB Connected successfully.');
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log('✅ MongoDB Connected');
   } catch (err) {
-    console.error('❌ MongoDB Connection Error:', err.message);
-    process.exit(1); // Exit process with failure
+    console.error('❌ MongoDB Error:', err.message);
+    process.exit(1);
   }
 };
 
